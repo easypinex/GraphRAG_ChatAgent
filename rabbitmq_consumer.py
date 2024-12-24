c25a8841f306717e623f769ae7a6947de80f9919
品至 <easypinex@gmail.com>
2024-12-24 12:21:03 +0800

    bug fix

--- rabbitmq_consumer.py
+++ rabbitmq_consumer.py
@@ -22,16 +22,17 @@ def main():
     rabbitmq_pass = os.environ.get('RABBITMQ_PASSWORD', None)
     rabbitmq_cert_verify = os.getenv("RABBITMQ_CERT_VERIFY", "true").lower() == "true"
     rabbitmq_ssl = os.getenv("RABBITMQ_SSL", "true").lower() == "true"
-    ssl_context = None
+    ssl_options = None
     if rabbitmq_ssl:
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         if not rabbitmq_cert_verify:
             ssl_context.check_hostname = False
             ssl_context.verify_mode = ssl.CERT_NONE
+        ssl_options = pika.SSLOptions(context=ssl_context)
     credentials = None
     if rabbitmq_user and rabbitmq_pass:
         credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_pass)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_host, rabbitmq_port, credentials=credentials, ssl_options=ssl_context))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_host, rabbitmq_port, credentials=credentials, ssl_options=ssl_options))
     channel = connection.channel()
     channel.queue_declare(queue=rabbitmq_channel)
 
