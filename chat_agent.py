e0f84109d3a16300b869cbc04fc450acead51164
品至 <easypinex@gmail.com>
2025-01-09 13:07:06 +0800

    調整chunk threshold

--- chat_agent.py
+++ chat_agent.py
@@ -33,7 +33,7 @@ embedding = AzureOpenAIEmbeddings(
 )
 local_search_retriever = get_localsearch_retriever(embedding)
 local_search_retriever_chain: Runnable = local_search_retriever | FileMetadataSearch()
-vector_retriever = get_baseline_retriever(embedding, score_threshold=0.75)
+vector_retriever = get_baseline_retriever(embedding, score_threshold=0.85)
 vector_retriever_chain: Runnable = vector_retriever | FileMetadataSearch()
     
 llm = AzureChatOpenAI(
